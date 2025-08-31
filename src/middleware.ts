48a34a504502cfebe0e971f094d2ebbbc98756ab
Rohit Mehta <165664435+negativ14@users.noreply.github.com>
2025-09-01 00:24:33 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("next-auth.session-token")?.value;
+  const token =
+    req.cookies.get("next-auth.session-token")?.value ||
+    req.cookies.get("__Secure-next-auth.session-token")?.value;
 
   if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/", req.url));
